9793885ee18192748581a6d650905b9523e539a1
User GCT <yuri.ouverney@hotmai.com>
2021-05-27 12:00:25 -0300

    quarto modulo

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -5,7 +5,7 @@ const database = require('../models')
 class PessoaController{
     // static => esse método faz com que a função "PegaTodasPessoas" possa ser chamada no código, então posso chamá-la sem ter que criar uma nova instância de classe, sem ter que criar um "new PessoaController". 
     // async => diz que esse método precisa esperar resolver alguns outros métodos internos que usaremos aqui dentro para então devolver a resposta. Faz parte da questão de sincronia e assincronia do JavaScript.
-    static async PegaTodasPessoas (req, res) {
+    static async pegaTodasPessoas (req, res) {
         // try/catch => funciona como o try, except do python
         try{
             //await => pedir para o JavaScript esperar ir até o banco, pegar as coisas e voltar, então ele vai dar um await, a palavra-chave que faz par com async.
@@ -16,7 +16,57 @@ class PessoaController{
         } catch(error) {
             return res.status(500).json(error.message)
         }
+    }
+    static async pegaUmaPessoa(req, res) {
+        // este id será da requisição feita. Exemplo: localhost:3000/pessoas/4
+        // o 4 seria este id
+        const { id } = req.params
+        try{
+            // aqui vai o id do banco de dados, o "number" é para não dar problema, retornando um número.
+            // primeiro id = coluna do banco de dados
+            // segundo id (id) = o parâmetro que recebe da requisição feita acima.
+            const umaPessoa = await database.Pessoas.findOne({ 
+                where: {
+                id:Number(id)} 
+            })
+            return res.status(200).json(umaPessoa)
+        } catch(error) {
+            return res.status(500).json(eror)
 
+        }
+    }
+    // create -- Post
+    static async criaPessoa(req, res){
+        const novaPessoa = req.body
+        try{
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(200).json(novaPessoaCriada)
+
+        }catch(error){
+            return res.status(500).json(error)
+        }
+    }
+    // update - PUT
+    static async atualizaPessoa(req, res){
+        const { id } = req.params
+        const novasInfos = req.body
+        try{
+            await database.Pessoas.update(novasInfos, { where:{id:Number(id)} })
+            const pessoaAtualizada = await database.Pessoas.findOne({ where:{id:Number(id)}})
+            return res.status(200).json(pessoaAtualizada)
+
+        }catch(error){
+            return res.status(500).json(error)
+        }
+    }
+    static async apagaPessoa(req, res){
+        const { id } = req.params
+        try{
+            await database.Pessoas.destroy({where:{id:Number(id)}})
+            return res.status(200).json({message: 'o Id ' +id+' foi deletado'})
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
     }
 }
 
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,7 +3,12 @@ const pessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
-//chamando o método PegaTodasPessoas de "/controllers/pessoaController"
-router.get('/pessoas', pessoaController.PegaTodasPessoas)
+//chamando o método pegaTodasPessoas de "/controllers/pessoaController"
+router.get('/pessoas', pessoaController.pegaTodasPessoas)
+//os ":"" indica o parâmetro que vai receber
+router.get('/pessoas/:id', pessoaController.pegaUmaPessoa)
+router.post('/pessoas/', pessoaController.criaPessoa)
+router.put('/pessoas/:id', pessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', pessoaController.apagaPessoa)
 
 module.exports = router
